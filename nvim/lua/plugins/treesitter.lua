defe0a7eeaa8a1deab9664a74a365531a9d43479
kiransilwal1 <kiran.silwal1@gmail.com>
2024-12-07 16:18:47 +0545

    fix the crash that appeared with fold from treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,9 +1,9 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+
     opts = {
       ensure_installed = {
-        "dart",
         "astro",
         "bash",
         "c",
@@ -41,10 +41,14 @@ return {
         "vimdoc",
         "yaml",
       },
+      highlight = {
+        enable = true, -- Keep Treesitter highlight enabled
+        additional_vim_regex_highlighting = false,
+      },
+      fold = { enable = false }, -- Fold setting placed directly
       config = function(_, opts)
         require("nvim-treesitter.configs").setup(opts)
-
-        -- MDX
+        -- MDX Support
         vim.filetype.add({
           extension = {
             mdx = "mdx",
@@ -62,3 +66,68 @@ return {
     enabled = false,
   },
 }
+
+-- return {
+--   {
+--     "nvim-treesitter/nvim-treesitter",
+--
+--     opts = {
+--       fold = { enable = false },
+--       ensure_installed = {
+--         "astro",
+--         "bash",
+--         "c",
+--         "cmake",
+--         "cpp",
+--         "css",
+--         "diff",
+--         "dockerfile",
+--         "gitignore",
+--         "go",
+--         "gomod",
+--         "gosum",
+--         "gowork",
+--         "html",
+--         "http",
+--         "javascript",
+--         "jsdoc",
+--         "json",
+--         "json5",
+--         "jsonc",
+--         "lua",
+--         "luadoc",
+--         "luap",
+--         "markdown",
+--         "markdown_inline",
+--         "prisma",
+--         "python",
+--         "query",
+--         "regex",
+--         "sql",
+--         "toml",
+--         "tsx",
+--         "typescript",
+--         "vim",
+--         "vimdoc",
+--         "yaml",
+--       },
+--       config = function(_, opts)
+--         require("nvim-treesitter.configs").setup(opts)
+--         -- MDX
+--         vim.filetype.add({
+--           extension = {
+--             mdx = "mdx",
+--           },
+--         })
+--         vim.treesitter.language.register("markdown", "mdx")
+--       end,
+--     },
+--   },
+--   {
+--     "nvim-treesitter/playground",
+--   },
+--   {
+--     "nvim-treesitter/nvim-treesitter-context",
+--     enabled = false,
+--   },
+-- }
