8c70bd67536f54ad5f1fb1684adb333eb5a19315
kiransilwal1 <kiran.silwal1@gmail.com>
2024-12-07 16:17:38 +0545

    remove origin luasnip functionality to overwrite

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -1,8 +1,18 @@
 return {
-  "L3MON4D3/LuaSnip",
-  opts = {
-    history = true,
-    region_check_events = "InsertEnter",
-    delete_check_events = "TextChanged,InsertLeave",
-  },
+  -- "L3MON4D3/LuaSnip",
+  -- opts = {
+  --   history = true,
+  --   region_check_events = "InsertEnter",
+  --   delete_check_events = "TextChanged,InsertLeave",
+  -- },
+  -- config = function(_, opts)
+  --   -- Configure LuaSnip with provided options
+  --   require("luasnip").config.setup(opts)
+  --
+  --   -- Extend TypeScript to include JavaScript snippets
+  --   require("luasnip").filetype_extend("typescript", { "javascript" })
+  --
+  --   -- Ensure snippets are loaded properly
+  --   require("luasnip.loaders.from_vscode").lazy_load()
+  -- end,
 }
