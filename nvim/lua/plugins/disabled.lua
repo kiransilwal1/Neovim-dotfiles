72e20834ff7f2871071367a091f53a670932ce9c
kiransilwal1 <kiran.silwal1@gmail.com>
2024-12-13 18:06:40 +0545

    surround feature addition

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,4 +1,11 @@
 return {
   -- Disable auto opening (){}[]
   { "echasnovski/mini.pairs", enabled = false },
+  { "kylechui/nvim-surround", enabled = false },
+  -- {
+  --   "folke/flash.nvim",
+  --   keys = {
+  --     { "s", false },
+  --   },
+  -- },
 }
---
+++ nvim/lua/plugins/nvim-surround.lua
@@ -0,0 +1,15 @@
+return {
+  "echasnovski/mini.surround",
+  opts = {
+    mappings = {
+      add = "as", -- Add surrounding in Normal and Visual modes
+      delete = "ds", -- Delete surrounding
+      find = "fs", -- Find surrounding (to the right)
+      find_left = "Fs", -- Find surrounding (to the left)
+      highlight = "hs", -- Highlight surrounding
+      replace = "rs", -- Replace surrounding
+      update_n_lines = "ns", -- Update `n_lines`
+    },
+  },
+  version = "*",
+}
