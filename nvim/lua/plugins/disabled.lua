cefe9df2962f0442ef7bbe8d8e4e0501c1715355
kiransilwal1 <kiran.silwal1@gmail.com>
2024-12-15 15:27:15 +0545

    Surround plugin workaround

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,7 +1,7 @@
 return {
   -- Disable auto opening (){}[]
   { "echasnovski/mini.pairs", enabled = false },
-  { "kylechui/nvim-surround", enabled = false },
+  -- { "kylechui/nvim-surround", enabled = false },
   -- {
   --   "folke/flash.nvim",
   --   keys = {
--- nvim/lua/plugins/nvim-surround.lua
+++ nvim/lua/plugins/nvim-surround.lua
@@ -1,15 +1,27 @@
 return {
-  "echasnovski/mini.surround",
-  opts = {
-    mappings = {
-      add = "as", -- Add surrounding in Normal and Visual modes
-      delete = "ds", -- Delete surrounding
-      find = "fs", -- Find surrounding (to the right)
-      find_left = "Fs", -- Find surrounding (to the left)
-      highlight = "hs", -- Highlight surrounding
-      replace = "rs", -- Replace surrounding
-      update_n_lines = "ns", -- Update `n_lines`
-    },
-  },
-  version = "*",
+  "kylechui/nvim-surround",
+  version = "*", -- Use for stability; omit to use `main` branch for the latest features
+  event = "VeryLazy",
+  config = function()
+    require("nvim-surround").setup({
+      -- Configuration here, or leave empty to use defaults
+    })
+  end,
 }
+
+--
+-- return {
+--   "echasnovski/mini.surround",
+--   opts = {
+--     mappings = {
+--       add = "as", -- Add surrounding in Normal and Visual modes
+--       delete = "ds", -- Delete surrounding
+--       find = "fs", -- Find surrounding (to the right)
+--       find_left = "Fs", -- Find surrounding (to the left)
+--       highlight = "hs", -- Highlight surrounding
+--       replace = "rs", -- Replace surrounding
+--       update_n_lines = "ns", -- Update `n_lines`
+--     },
+--   },
+--   version = "*",
+-- }
